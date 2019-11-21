2931fb1d3e4151688d26fa87024818cb7637e18b
Andrew Ribeiro <andtankian@gmail.com>
2019-11-21 14:09:39 -0300

    Improved, added missing "build" script to package.json file and adusted some unused imports

--- src/lib/components/Styles.tsx
+++ src/lib/components/Styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components'
 import { createThemeWithAppearance } from '@redwallsolutions/theming-component-module'
+import { ICommonProps } from '@redwallsolutions/common-interfaces-ts'
 
 const theme = createThemeWithAppearance()
